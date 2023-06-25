7767703d489669cbafe81038207547ebd7da8d2c
NoaCo99 <137066345+NoaCo99@users.noreply.github.com>
2023-06-25 23:55:56 +0300

    Add files via upload

---
+++ jScripts/JavaScript.js
@@ -0,0 +1,37 @@
+﻿var slideNum = 1;
+picVisible(slideNum);
+
+function moveBTN(i) {
+    picVisible(slideNum += i);
+}
+
+function choosedPic(i) {
+    picVisible(slideNum = i);
+}
+
+function picVisible(i) {
+    var n;
+    var slides = document.getElementsByClassName("photos");
+    var points = document.getElementsByClassName("demo");
+    var text = document.getElementById("imgDes");
+
+    if (i > slides.length) {
+        slideNum = 1;
+    }
+
+    if (i < 1) {
+        slideNum = slides.length;
+    }
+
+    for (n = 0; n < slides.length; n++) {
+        slides[n].style.display = "none";
+    }
+
+    for (n = 0; n < points.length; n++) {
+        points[n].className = points[n].className.replace(" active", "");
+    }
+
+    slides[slideNum - 1].style.display = "block";
+    points[slideNum - 1].className += " active";
+    text.innerHTML = points[slideNum - 1].alt;
+}
